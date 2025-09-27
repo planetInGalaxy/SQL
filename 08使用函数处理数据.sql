c5c56965e41fb5e8055df767d409a095622e9e36
jiaguo <56522923+planetInGalaxy@users.noreply.github.com>
2025-09-27 11:35:00 +0800

    Update 08使用函数处理数据.sql

--- 08使用函数处理数据.sql
+++ 08使用函数处理数据.sql
@@ -34,11 +34,11 @@ SELECT vend_name,
     LEFT(vend_name, 2) AS vend_name_left
 FROM Vendors
 ORDER BY vend_name_left;
-# Oracle,MySQL中的日期查找函数
+# 日期查找函数
 SELECT order_num
 FROM Orders
 WHERE EXTRACT(year FROM order_date) = 2020;
-# MySQL中的YEAR函数，提取年份
+# YEAR函数，提取年份
 SELECT order_num
 FROM Orders
 WHERE YEAR(order_date) = '2020';
@@ -70,54 +70,15 @@ WHERE SOUNDEX(cust_contact) = SOUNDEX('Michael Green');
 
 /*挑战题*/
 # 1
--- MySQL
 SELECT cust_id, cust_name,
         CONCAT(UPPER(LEFT(cust_contact, 2)), 
         UPPER(LEFT(cust_city, 3))) AS user_login
 FROM customers;
--- DB2, PostgreSQL
-SELECT cust_id, cust_name,
-        UPPER(LEFT(cust_contact, 2)) 
-        || UPPER(LEFT(cust_city, 3)) AS user_login
-FROM customers;
--- Oracle, SQLite
-SELECT cust_id, cust_name,
-        UPPER(SUBSTR(cust_contact, 1, 2)) 
-        || UPPER(SUBSTR(cust_city, 1, 3)) 
-        AS user_login
- FROM customers;
--- SQL Server
-SELECT cust_id, cust_name,
-        UPPER(LEFT(cust_contact, 2)) 
-        + UPPER(LEFT(cust_city, 3)) AS user_login
-FROM customers;
+
 # 2
--- MySQL, DB2, MariaDB
 SELECT order_num, order_date
 FROM Orders
 WHERE YEAR(order_date) = 2020 
     AND MONTH(order_date) = 1
 ORDER BY order_date;
--- Oracle, PostgreSQL
-SELECT order_num, order_date
-FROM Orders
-WHERE EXTRACT(year FROM order_date) = 2020 
-    AND EXTRACT(month FROM order_date) = 1
-ORDER BY order_date;
--- PostgreSQL
-SELECT order_num, order_date
-FROM Orders
-WHERE DATE_PART('year', order_date) = 2020 
-    AND DATE_PART('month', order_date) = 1
-ORDER BY order_num;
--- SQL Server
-SELECT order_num, order_date
-FROM Orders
-WHERE DATEPART(yy, order_date) = 2020 
-    AND DATEPART(mm, order_date) = 1
-ORDER BY order_date;
--- SQLite
-SELECT order_num
-FROM Orders
-WHERE strftime('%Y', order_date) = '2020' 
-    AND strftime('%m', order_date) = '01';
+
